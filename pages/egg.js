345bf89feb7854ce444bfc4de3a9ae3fa91f8acc
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-08-26 16:34:32 +0200

    Update page to test CI

--- pages/egg.js
+++ pages/egg.js
@@ -1,5 +1,11 @@
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <div className={styles.container}>This is an egg</div>;
+  return (
+    <>
+      <div className={styles.container}>This is an egg two times bigger</div>
+      <div className={styles.container}>This is an egg thre times bigger</div>
+      <div className={styles.container}>This is an cookie for you</div>
+    </>
+  );
 }
