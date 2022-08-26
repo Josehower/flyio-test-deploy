231fd11ed4f3eacb9f0af31a089fe70fc688f59a
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-08-26 18:49:19 +0200

    Add ley config

---
+++ ley.config.js
@@ -0,0 +1,11 @@
+const setEnvironments = require('./util/setEnvironments');
+
+setEnvironments();
+
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = { rejectUnauthorized: false };
+}
+
+module.exports = options;
