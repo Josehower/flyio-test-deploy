16a5da7b1063ac039a522e388823fc3d607c3036
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-08-30 18:35:41 +0200

    Add database egg

--- pages/egg.js
+++ pages/egg.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 export default function Home(props) {
   return (
     <>
-      {/* {JSON.stringify(props.users)} */}
+      {JSON.stringify(props.users)}
       <div className={styles.container}>This is an egg two times bigger</div>
       <div className={styles.container}>This is an egg thre times bigger</div>
       <div className={styles.container}>This is an cookie for you</div>
@@ -12,14 +12,14 @@ export default function Home(props) {
   );
 }
 
-// export async function getServerSideProps() {
-//   const users = await getUsers();
-//   return {
-//     // Anything that you pass in the props
-//     // object will get passed to the component
-//     // at the top in the `props` parameter
-//     props: {
-//       users: users,
-//     },
-//   };
-// }
+export async function getServerSideProps() {
+  const users = await getUsers();
+  return {
+    // Anything that you pass in the props
+    // object will get passed to the component
+    // at the top in the `props` parameter
+    props: {
+      users: users,
+    },
+  };
+}
